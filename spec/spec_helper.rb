ca1623f326c7d47a366e0fd7a825fb7e1815d83a
Dan Molloy <danieljmolloy1@gmail.com>
2017-05-26 20:58:08 +0100

    init

---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require File.expand_path("../../lib/invalid_base_permissions_error", __FILE__)
+require File.expand_path("../../lib/permission_dependency_resolver", __FILE__)
+
+RSpec.configure do |config|
+  config.color = true
+  config.add_formatter 'documentation'
+end
