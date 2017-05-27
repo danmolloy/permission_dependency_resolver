85fe2b14df8b7bdf09d72e55bc1e6d069c6eaf61
Dan Molloy <danieljmolloy1@gmail.com>
2017-05-27 12:45:33 +0100

    test refactor

--- spec/permission_dependency_resolver_spec.rb
+++ spec/permission_dependency_resolver_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe PermissionDependencyResolver do
-
   let(:simple_permission_dependencies) do
     {
       'view' => [],
@@ -11,63 +10,68 @@ describe PermissionDependencyResolver do
       'delete' => ['edit']
     }
   end
-
-  it 'validates whether permissions can be granted given simple dependencies' do
-    pdr = PermissionDependencyResolver.new(simple_permission_dependencies)
-    expect(pdr.can_grant?(['view'], 'edit')).to eq true
-    expect(pdr.can_grant?(['view'], 'delete')).to eq false
-    expect(pdr.can_grant?(['view', 'edit'], 'alter_tags')).to eq true
-    expect(pdr.can_grant?(['view'], 'create')).to eq true
-  end
-
-  it 'can sort permissions in dependency order given simple dependencies' do
-    pdr = PermissionDependencyResolver.new(simple_permission_dependencies)
-
-    expect(pdr.sort(['edit', 'delete', 'view'])).to eq ['view', 'edit', 'delete']
-    valid_orderings = [['view', 'create', 'edit', 'alter_tags'],
-                       ['view', 'edit', 'create', 'alter_tags'],
-                       ['view', 'edit', 'alter_tags', 'create']]
-    expect(valid_orderings).to include(pdr.sort(['create', 'alter_tags', 'view', 'edit']))
-  end
-
-  it 'validates whether permissions can be denied given simple dependencies' do
-    pdr = PermissionDependencyResolver.new(simple_permission_dependencies)
-
-    expect(pdr.can_deny?(['view', 'edit'], 'view')).to eq false
-    expect(pdr.can_deny?(['view', 'edit'], 'edit')).to eq true
-    expect(pdr.can_deny?(['view', 'edit', 'create'], 'edit')).to eq true
-    expect(pdr.can_deny?(['view', 'edit', 'delete'], 'edit')).to eq false
-  end
-
   let(:complex_permission_dependencies) do
     simple_permission_dependencies.merge({
       'audit' => ['create', 'delete'],
       'batch_update' => ['edit', 'create']
     })
   end
+  let(:simple_pdr)  {PermissionDependencyResolver.new(simple_permission_dependencies)}
+  let(:complex_pdr) {PermissionDependencyResolver.new(complex_permission_dependencies)}
 
-  it 'validates whether permissions can be granted given complex dependencies' do
-    pdr = PermissionDependencyResolver.new(complex_permission_dependencies)
-    expect(pdr.can_grant?(['view', 'edit', 'delete'], 'batch_update')).to eq false
-    expect(pdr.can_grant?(['view', 'edit', 'create'], 'batch_update')).to eq true
-    expect(pdr.can_grant?(['view', 'edit', 'delete'], 'audit')).to eq false
-    expect(pdr.can_grant?(['view', 'edit', 'delete', 'create'], 'audit')).to eq true
+  describe '#can_grant?' do
+    it 'validates whether permissions can be granted given simple dependencies' do
+      expect(simple_pdr.can_grant?(['view'], 'edit')).to eq true
+      expect(simple_pdr.can_grant?(['view'], 'delete')).to eq false
+      expect(simple_pdr.can_grant?(['view', 'edit'], 'alter_tags')).to eq true
+      expect(simple_pdr.can_grant?(['view'], 'create')).to eq true
+    end
+    it 'validates whether permissions can be granted given complex dependencies' do
+      expect(complex_pdr.can_grant?(['view', 'edit', 'delete'], 'batch_update')).to eq false
+      expect(complex_pdr.can_grant?(['view', 'edit', 'create'], 'batch_update')).to eq true
+      expect(complex_pdr.can_grant?(['view', 'edit', 'delete'], 'audit')).to eq false
+      expect(complex_pdr.can_grant?(['view', 'edit', 'delete', 'create'], 'audit')).to eq true
+    end
+    it 'throws an exception when validating permissions if existing permissions are invalid' do
+      expect{ complex_pdr.can_grant?(['edit', 'create'], 'alter_tags') }.to raise_error(InvalidBasePermissionsError)
+    end
   end
 
-  it 'throws an exception when validating permissions if existing permissions are invalid' do
-    pdr = PermissionDependencyResolver.new(complex_permission_dependencies)
-    expect{ pdr.can_grant?(['edit', 'create'], 'alter_tags') }.to raise_error(InvalidBasePermissionsError)
-    expect{ pdr.can_deny?(['create', 'delete'], 'audit') }.to raise_error(InvalidBasePermissionsError)
+  describe '#can_deny?' do
+    it 'validates whether permissions can be denied given simple dependencies' do
+      expect(simple_pdr.can_deny?(['view', 'edit'], 'view')).to eq false
+      expect(simple_pdr.can_deny?(['view', 'edit'], 'edit')).to eq true
+      expect(simple_pdr.can_deny?(['view', 'edit', 'create'], 'edit')).to eq true
+      expect(simple_pdr.can_deny?(['view', 'edit', 'delete'], 'edit')).to eq false
+    end
+    it 'validates whether permissions can be denied given complex dependencies' do
+      expect(complex_pdr.can_deny?(['view', 'edit', 'create', 'delete', 'audit'], 'audit')).to eq true
+      expect(complex_pdr.can_deny?(['view', 'edit', 'create', 'delete', 'audit'], 'delete')).to eq false
+      expect(complex_pdr.can_deny?(['view', 'edit', 'create', 'batch_update'], 'batch_update')).to eq true
+      expect(complex_pdr.can_deny?(['view', 'edit', 'create', 'batch_update'], 'view')).to eq false
+    end
+    it 'throws an exception when validating permissions if existing permissions are invalid' do
+      expect{ complex_pdr.can_deny?(['create', 'delete'], 'audit') }.to raise_error(InvalidBasePermissionsError)
+    end
   end
 
-  it 'can sort permissions in dependency order given complex dependencies' do
-    pdr = PermissionDependencyResolver.new(complex_permission_dependencies)
-    possible_orderings = [
-      ['view', 'edit', 'create', 'delete', 'audit'],
-      ['view', 'create', 'edit', 'delete', 'audit'],
-      ['view', 'edit', 'delete', 'create', 'audit']
-    ]
-    expect(possible_orderings).to include(pdr.sort(['audit', 'create', 'delete', 'view', 'edit']))
+  describe '#sort' do
+    it 'can sort permissions in dependency order given simple dependencies' do
+      valid_orderings = [
+        ['view', 'create', 'edit', 'alter_tags'],
+        ['view', 'edit', 'create', 'alter_tags'],
+        ['view', 'edit', 'alter_tags', 'create']
+      ]
+      expect(simple_pdr.sort(['edit', 'delete', 'view'])).to eq ['view', 'edit', 'delete']
+      expect(valid_orderings).to include(simple_pdr.sort(['create', 'alter_tags', 'view', 'edit']))
+    end
+    it 'can sort permissions in dependency order given complex dependencies' do
+      valid_orderings = [
+        ['view', 'edit', 'create', 'delete', 'audit'],
+        ['view', 'create', 'edit', 'delete', 'audit'],
+        ['view', 'edit', 'delete', 'create', 'audit']
+      ]
+      expect(valid_orderings).to include(complex_pdr.sort(['audit', 'create', 'delete', 'view', 'edit']))
+    end
   end
-
 end
